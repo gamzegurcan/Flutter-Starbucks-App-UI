52282b5efcd83cdc7fb47e3c0aae8842f21f3282
Gamze Gürcan <gmzgrcn18812@icloud.com>
2022-07-13 01:45:32 +0300

    ...

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 import 'package:starbucks_app/core/constant/colors/app_colors.dart';
 import 'package:starbucks_app/core/constant/text/app_text.dart';
-import 'package:starbucks_app/product/custom_app_bar.dart';
+import 'package:starbucks_app/product/widget/custom_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
